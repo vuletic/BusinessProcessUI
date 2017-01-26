616908b358494575da4b5c8d1beaf38a904aa555
vuletic <vuletic93@gmail.com>
2017-01-26 16:31:49 +0100

    Initial

---
+++ app/components/home/home.controller.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    'use strict';
+
+    angular
+		.module('app')
+		.controller('homeController', homeController);
+
+	homeController.$inject = ['$http'];
+    function homeController($http) {
+        var hc = this;
+        hc.home = "Home";
+
+        $http.get("http://kermit:kermit@localhost:8080/activiti-rest/service/identity/groups").then(function (response) {
+                alert(JSON.stringify(response.data));
+            });
+
+    }
+
+
+})();
